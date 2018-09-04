ad3b3db7984a2bd2fd2aea79445cb892e58a73c6
Samuel NEVEU <saneveu@e3r2p8.42.fr>
2018-09-04 16:28:20 +0200

    daw

--- d05/ex10/ft_strcapitalize.c
+++ d05/ex10/ft_strcapitalize.c
@@ -6,23 +6,40 @@
 /*   By: saneveu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/03 21:58:58 by saneveu           #+#    #+#             */
-/*   Updated: 2018/09/03 22:54:52 by saneveu          ###   ########.fr       */
+/*   Updated: 2018/09/04 16:27:00 by saneveu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_capitalize(char *str)
+#define MAJ(c) (c >= 65 && c <= 90 ? 1 : 0) 
+#define MIN(c) (c >= 'a' && c <= 'z' ? 1 : 0)
+#define LETTER(c) (MAJ(c) || MIN(c) ? 1 : 0)
+#define NUMBER(c) (c >= '0' && c <= '9' ? 1 : 0)
+
+char	*ft_strcapitalize(char *str)
 {
 	int i;
 
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] >= 65 && str[i] <= 90)
+		if (LETTER(str[i]) && MAJ(str[i]))
+		{
 			str[i] += 32;
-		i++;
+			i++;
+		}
+		else
+			i++;
 	}
+	
 	i = 0;
 	while (str[i])
 	{
-		if (str[i - 1] == ' ' || str[i - 1] < 97 || str[i - 1] > 123)
-			
+		if (MIN(str[i]))
+		{
+			if ((!NUMBER(str[i - 1])) && (!LETTER(str[i - 1])))
+				str[i] -= 32;
+		}
+		i++;
+	}
+	return (str);
+}	
