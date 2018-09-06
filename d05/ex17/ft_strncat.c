ec29953430132eeeca1ae84517d40160bd3e3b09
Samuel NEVEU <saneveu@e3r11p7.42.fr>
2018-09-06 18:16:09 +0200

    la vie c'est cool

--- d05/ex17/ft_strncat.c
+++ d05/ex17/ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: saneveu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/04 18:01:21 by saneveu           #+#    #+#             */
-/*   Updated: 2018/09/06 14:29:13 by saneveu          ###   ########.fr       */
+/*   Updated: 2018/09/06 18:14:15 by saneveu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,16 @@ int		ft_strlen(char *str)
 
 char	*ft_strncat(char *dest, char *src, int nb)
 {
-	int i;
-	int j;
+	int		i;
+	char	*str;
 
 	i = 0;
-	j = ft_strlen(dest);
-	while (i <= nb)
+	str = dest + ft_strlen(dest);
+	while (src[i] && i <= nb)
 	{
-		dest[j] = src[i];
+		dest[i] = src[i];
 		i++;
-		j++;
 	}
-	dest[j] = '\0';
+	str[i] = '\0';
 	return (dest);
 }
--- d09/ex00/ft_generic.c
+++ d09/ex00/ft_generic.c
@@ -1,35 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_range.c                                         :+:      :+:    :+:   */
+/*   ft_generic.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: saneveu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/09/05 21:13:54 by saneveu           #+#    #+#             */
-/*   Updated: 2018/09/06 14:42:52 by saneveu          ###   ########.fr       */
+/*   Created: 2018/09/06 17:16:00 by saneveu           #+#    #+#             */
+/*   Updated: 2018/09/06 17:18:21 by saneveu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include <unistd.h>
 
-int		*ft_range(int min, int max)
+void	ft_generic(void)
 {
-	int *dest;
-	int taille;
-	int i;
-	int j;
-
-	j = 0;
-	i = min - 1;
-	taille = max - min;
-	if (min >= max)
-		return (0);
-	if(!(dest = malloc(sizeof(taille))))
-		return (0);
-	while (i++ < (max - 1))
-	{	
-		dest[j] = i;
-		j++;
-	}
-	return (dest);
+	write(1, "Tut tut ; Tut tut", 17);
 }
