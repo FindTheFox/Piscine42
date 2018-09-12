0e94d83e0845bb87d35ff01867e51e1672eecc7a
FindTheFox <samuel.neveugall@yahoo.fr>
2018-09-12 16:39:41 +0200

    cantina

---
+++ d08/ex05/ft_show_tab.c
@@ -0,0 +1,32 @@
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void	ft_print_words_tables(char **str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putstr(str[i]);
+		ft_putchar('\n');
+		i++;
+	}
+}
+
+void	ft_show_tab(struct s_stock_par *par)
+{
+	int i;
+
+
